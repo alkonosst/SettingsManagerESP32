372fde81ef07a63d6867d7fc9a6efe8b9c2cdb06
alkonosst <m.ramirez.bravo.94@gmail.com>
2025-01-07 12:37:55 -0300

    chore: Bump version

--- src/SettingsManagerESP32.h
+++ src/SettingsManagerESP32.h
@@ -1,5 +1,5 @@
 /**
- * SPDX-FileCopyrightText: 2024 Maximiliano Ramirez <maximiliano.ramirezbravo@gmail.com>
+ * SPDX-FileCopyrightText: 2025 Maximiliano Ramirez <maximiliano.ramirezbravo@gmail.com>
  *
  * SPDX-License-Identifier: GPL-3.0-or-later
  */
