cc7c28b2569ae6a5430a1f6bf83d996886c60b5a
alkonosst <m.ramirez.bravo.94@gmail.com>
2025-11-22 00:44:42 -0300

    refactor: Change library license, add funding file, bump version

--- examples/Strings/Strings.ino
+++ examples/Strings/Strings.ino
@@ -1,7 +1,7 @@
 /**
  * SPDX-FileCopyrightText: 2025 Maximiliano Ramirez <maximiliano.ramirezbravo@gmail.com>
  *
- * SPDX-License-Identifier: GPL-3.0-or-later
+ * SPDX-License-Identifier: MIT
  */
 
 /** Explanation of the example:
